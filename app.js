ff539e038b993cc3b38fc1e057b9c163a09a7d04
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-10-24 13:53:46 +0300

    x

---
+++ app.js
@@ -0,0 +1,27 @@
+
+const darkbtn = document.getElementById('.dark');
+const body = document.querySelector('body');
+darkbtn.addEventListener('click', ()=>{
+    body.classList.add('dark')
+})
+
+// const list = document.querySelector('ul');
+
+// const title = document.querySelector('h2');
+
+// const data = ['ali', 'axror', 'dilshod'];
+
+// const result = data.find(name => name === 'axror');
+// console.log(result);  // "axror"
+
+
+// const result = data[1];
+// console.log(result);  // "axror"
+
+
+
+// const upperCaseData = data.map(name => name.toUpperCase());
+// console.log(upperCaseData);  // ["ALI", "AXROR", "DILSHOD"]
+
+
+
