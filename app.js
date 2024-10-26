d46cdc19cf43eaaf99fa2d41b95eb41e360c6870
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-10-26 13:34:06 +0300

    password ture and fols

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-const data = ['ali', 'Vali', 'gani'];
-const ism = prompt();
+// const data = ['ali', 'Vali', 'gani'];
+// const ism = prompt();
 
-if(data.filter((e)=> e === ism).length > 0){
-    console.log(ism);
+// if(data.filter((e)=> e === ism).length > 0){
+//     console.log(ism);
     
-}else{
-    console.log('xato');
+// }else{
+//     console.log('xato');
     
-}
+// }
+
+
+// const correctPassword = "12345"; // Ilovaning parolini shu yerda belgilang
+// // Foydalanuvchining parolini shu yerda tekshiring
+// const userPassword = prompt("Parolingizni kiriting:");
+// Password(userPassword);
+
+// function Password(userPassword) {
+//   if (userPassword === correctPassword) {
+//     console.log("Welcome");
+//   } else {
+//     console.error("Error: Parol noto'g'ri");
+//   }
+// }
+
+
