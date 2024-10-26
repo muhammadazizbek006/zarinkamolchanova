42b0b05bd83adf30464b56f1f4a410168d9f58fa
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-10-26 08:35:04 +0300

    foydalanuvchi ism kiritadi agar ism datada bo'lsa ism chiqadi agar bo'lmasa xato beradi

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+const data = ['ali', 'Vali', 'gani'];
+const ism = prompt();
 
-
-
-
+if(data.filter((e)=> e === ism).length > 0){
+    console.log(ism);
+    
+}else{
+    console.log('xato');
+    
+}
