6f7ae140e93b94ec11047984761a5530473b18d7
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-10-27 08:06:09 +0300

    number data random function

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
+// let title = document.querySelector('h2');
+// const minus = document.querySelector('#minus');
+// const span = document.querySelector('span');
+// const pilus = document.querySelector('#pilus');
+
+// let i = 1
+
+// pilus.addEventListener('click', ()=>{
+//     span.textContent = i++;
+// })
+
+
+//     minus.addEventListener('click', ()=>{
+//         if(i > 1){
+//         span.textContent = i--;
+//     }
+//     })
+
+
+// if(document.body.classList.add('gooo')){
+//     btn.addEventListener('click', ()=> {
+//         document.body.classList.remove('gooo')
+//     })
+// }else{(document.body.classList.remove('gooo'))
+
+//     btn.addEventListener('click', ()=> {
+//         document.body.classList.add('gooo')
+//     })
+// }
+
+
 // const data = ['ali', 'Vali', 'gani'];
 // const ism = prompt();
 
@@ -24,3 +55,15 @@
 // }
 
 
+
+
+
+const numberData = [1, 2, 3, 4, 5, 6];
+let son = Number(prompt("Son kiriting:"));
+let randomSon = numberData[Math.floor(Math.random() * numberData.length)];
+
+document.body.innerHTML = `
+  <p>Massivdagi sonlar: ${numberData.join(', ')}</p>
+  <p>${son === randomSon ? "Tabriklaymiz, yutdingiz!" : "Afsus, yutolmadingiz."}</p>
+  <p>Tanlangan son: ${randomSon}</p>
+`;
